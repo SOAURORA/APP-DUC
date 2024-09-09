346e540b050ed7bc8b8402a9b0ca5354ed78896a
SOAURORA <134520375+SOAURORA@users.noreply.github.com>
2024-09-09 15:31:43 -0300

    APP-UP2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { chevronForward, listCircle } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage {
 
   constructor() {}
   menuType: string = 'push';
 }
+
