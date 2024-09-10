e91e244e31cfb459f4f7e592d0b2091f75fda02f
SOAURORA <134520375+SOAURORA@users.noreply.github.com>
2024-09-09 21:02:14 -0300

    UPGRADE2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,16 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'password-change',
+    loadChildren: () => import('./password-change/password-change.module').then( m => m.PasswordChangePageModule)
   },
 ];
 
