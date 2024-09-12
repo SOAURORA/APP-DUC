f02d9cbff7ebee2108ab3ecf729c357c107b2c3a
SOAURORA <134520375+SOAURORA@users.noreply.github.com>
2024-09-12 14:22:55 -0300

    UPGRADE

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -17,4 +17,22 @@ export class LoginPage implements OnInit {
     password: '',
   };
 
+
+  burbuja=false;
+  this.burbuja= !this.burbuja
+  validar () {
+    if (this.user.username.length != 0) {
+      if (this.user.password.length != 0) {
+        console.log('Datos confirmados con exito.')
+
+      } else {
+        console.log('Sin contraseña valida')
+      }
+    } else {
+      console.log('Sin usuario valido')
+    }
+  }
+
+
+
 }
