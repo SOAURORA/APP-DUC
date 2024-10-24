86822992ac932d3eca7de2b7b1451a6d0b0b3f0a
SOAURORA <134520375+SOAURORA@users.noreply.github.com>
2024-10-24 17:00:09 -0300

    UPGRADE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },  {
+    path: 'error',
+    loadChildren: () => import('./error/error.module').then( m => m.ErrorPageModule)
   },
+
 ];
 
 @NgModule({
