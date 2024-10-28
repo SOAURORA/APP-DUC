fc65e4d5bfb3b067eeabaa925b1604ea96141764
SOAURORA <134520375+SOAURORA@users.noreply.github.com>
2024-10-27 22:23:12 -0300

    UPGRADE
    meh

--- src/app/Servicios/apicontroller.service.ts
+++ src/app/Servicios/apicontroller.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApicontrollerService {
+export class APIControllerService {
+  private baseUrl: string = 'http:/localhost:8100'; 
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  async loadUsers(): Promise<User[]> {
+    const users = await this.http.get<User[]>(`${this.baseUrl}/users`).toPromise();
+    return users || []; 
+  }
+
+  addUser(username: string, email: string, password: string, fechaNacimiento: string, rut: string): Promise<void> {
+    return this.http.post<void>(`${this.baseUrl}/users`, { username, email, password, fechaNacimiento, rut }).toPromise();
+  }
+
+  modifyUser(id: number, username: string, email: string, password: string, fechaNacimiento: string, rut: string): Observable<void> {
+    return this.http.put<void>(`${this.baseUrl}/users/${id}`, { username, email, password, fechaNacimiento, rut });
+  }
+
+  deleteUser(id: number): Promise<void> {
+    return this.http.delete<void>(`${this.baseUrl}/users/${id}`).toPromise();
+  }
 }
